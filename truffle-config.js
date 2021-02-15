2bf60fd937367f0052139b34ff64cac39c0eeb9f
albertov19 <albertoviera@gmail.com>
2021-02-15 10:15:16 +0000

    Update to HD-Wallet-Provider

--- truffle-config.js
+++ truffle-config.js
@@ -1,10 +1,12 @@
-const PrivateKeyProvider = require('./private-provider');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 // Standalone Development Node Private Key
 const privateKeyDev =
    '99B3C12287537E38C90A9219D4CB074A89A16E9CDB20BF85728EBD97C343E342';
 // Moonbase Alpha Private Key --> Please change this to your own Private Key with funds
+// NOTE: Do not store your private key in plaintext files
+//       this is only for demostration purposes only
 const privateKeyMoonbase =
-   '';
+   'YOUR_PRIVATE_KEY_HERE_ONLY_FOR_DEMOSTRATION_PURPOSES';
 
 module.exports = {
    networks: {
@@ -12,9 +14,14 @@ module.exports = {
       dev: {
          provider: () => {
             if (!privateKeyDev.trim()) {
-               throw new Error('Please enter a private key with funds, you can use the default one');
+               throw new Error(
+                  'Please enter a private key with funds, you can use the default one'
+               );
             }
-            return new PrivateKeyProvider(privateKeyDev, 'http://localhost:9933/', 1281)
+            return new HDWalletProvider(
+               privateKeyDev,
+               'http://localhost:9933/'
+            );
          },
          network_id: 1281,
       },
@@ -22,12 +29,19 @@ module.exports = {
       moonbase: {
          provider: () => {
             if (!privateKeyMoonbase.trim()) {
-               throw new Error('Please enter a private key with funds to send transactions to TestNet');
+               throw new Error(
+                  'Please enter a private key with funds to send transactions to TestNet'
+               );
             }
             if (privateKeyDev == privateKeyMoonbase) {
-               throw new Error('Please change the private key used for Moonbase to your own with funds');
+               throw new Error(
+                  'Please change the private key used for Moonbase to your own with funds'
+               );
             }
-            return new PrivateKeyProvider(privateKeyMoonbase, 'https://rpc.testnet.moonbeam.network', 1287)
+            return new HDWalletProvider(
+               privateKeyMoonbase,
+               'https://rpc.testnet.moonbeam.network'
+            );
          },
          network_id: 1287,
       },
@@ -35,9 +49,9 @@ module.exports = {
    // Solidity 0.7.0 Compiler
    compilers: {
       solc: {
-        version: "^0.7.0"
-      }
+         version: '^0.7.0',
+      },
    },
    // Moonbeam Truffle Plugin
-   plugins: ['moonbeam-truffle-plugin']
+   plugins: ['moonbeam-truffle-plugin'],
 };
